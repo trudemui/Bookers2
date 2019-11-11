f66a3b4a92e881f370a845618c4b6deb13bb0e48
trudemui <alesth_130@yahoo.co.jp>
2019-11-11 17:29:39 +0900

    install mysql2 and edit puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -42,6 +42,8 @@ stdout_redirect(
   File.join(rails_root, 'log', 'puma-error.log'),
   true
 )
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+rails_root = Dir.pwd
 # 本番環境のみデーモン起動
 if Rails.env.production?
     pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
